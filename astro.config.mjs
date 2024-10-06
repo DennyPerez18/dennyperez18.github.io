8fad472d8ae92556bd22d4ce72fa6f54ed98ee9f
UserIsntAvailable <pinedap.angel@gmail.com>
2024-10-06 09:11:13 -0400

    chore: add `prettierrc.mjs` and reformat all files.
    Somehow I never pushed this to source control, and was having differences on
    formatting between machines...
    
    fix: import correct type.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dennyperez.dev/",
-  integrations: [mdx(), sitemap(), tailwind()],
+    site: "https://dennyperez.dev/",
+    integrations: [mdx(), sitemap(), tailwind()],
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,19 @@
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-  // Type-check frontmatter using a schema
-  schema: ({ image }) => z.object({
-    title: z.string(),
-    description: z.string().optional(),
-    // Transform string to Date object
-    pubDate: z
-      .string()
-      .or(z.date())
-      .transform((val) => new Date(val)),
-    heroImage: image(),
-    categories: z.string().array(),
-  }),
+    // Type-check frontmatter using a schema
+    schema: ({ image }) =>
+        z.object({
+            title: z.string(),
+            description: z.string().optional(),
+            // Transform string to Date object
+            pubDate: z
+                .string()
+                .or(z.date())
+                .transform((val) => new Date(val)),
+            heroImage: image(),
+            categories: z.string().array(),
+        }),
 });
 
 export const collections = { blog };
