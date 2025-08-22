ad7cf9ac4f96773f57472b16cc10aaca82507019
UserIsntAvailable <pinedap.angel@gmail.com>
2025-08-22 08:40:37 -0400

    feat(Footer): add more contact links
    Also modify copyright message.
    
    fix: "Perez" typo
    
    chore: format `astro.config.mjs`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,17 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://dennyperez.dev/",
+    integrations: [mdx(), sitemap()],
     markdown: {
         shikiConfig: {
             theme: "catppuccin-latte",
         },
     },
-
-    site: "https://dennyperez.dev/",
-    integrations: [mdx(), sitemap()],
-
     vite: {
         plugins: [tailwindcss()],
     },
